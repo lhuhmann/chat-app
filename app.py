797536aa64d8fa793c367af66e1c05bd276acf13
bhuhmann <bhuhmann@mit.edu>
2021-01-10 13:48:21 -0500

    Display full list of channels when user returns to webpage after closing

--- app.py
+++ app.py
@@ -8,16 +8,24 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-# initialize channel list and
-# start with a general channel
+# when app is first started, define only general channel
 channels = ['general']
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@socketio.on("submit channel")
-def list_channels(data):
-    channels.append(data) if data not in channels else channels
-    print(channels, file=sys.stderr)
-    emit("announce", {"channels": channels}, broadcast=True)
+# @socketio.on("get channels")
+# def send_channels():
+#     emit("send channels", {"channels": channels}, broadcast=True)
+
+@socketio.on("update channels")
+def update_channels(new_channel = None):
+    if new_channel:
+        channels.append(new_channel) if new_channel not in channels else channels
+    emit("send channels", {"channels": channels}, broadcast=True)
+
+# @socketio.on("submit channel")
+# def list_channels(data):
+#     print(data, file=sys.stderr)
+#     emit("announce", {"channels": data}, broadcast=True)
--- static/index.js
+++ static/index.js
@@ -1,32 +1,50 @@
+function display_channels(channels) {
+    console.log("displaying channels:")
+    console.log(channels)
+    // clear list of channels
+    document.getElementById("channels_list").innerHTML = "";
+    // show list of channels
+    channels.forEach(function (item) {
+        var ul = document.getElementById('channels_list');
+        var li = document.createElement("li");
+        li.innerHTML += item;
+        ul.appendChild(li);
+    });
+};
+
 document.addEventListener('DOMContentLoaded', () => {
+    // Set starting channels to contain only general channel
+    // if (!localStorage.getItem('user'))
+    //     localStorage.setItem('channels', JSON.stringify(['general']));
+
+    // Display current value of channels
+    // display_channels(JSON.parse(localStorage.getItem('channels')));
+
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
     // When connected, configure buttons
     socket.on('connect', () => {
+        // upon connecting, get updated list of channels
+        socket.emit('update channels');
+
         // When submit_channel button is clicked,
-        // emit a submit channel event with the
+        // emit an update channels event with the
         // channel name
         document.querySelector('#channel_submit').onclick = function() {
-            const channel_name = document.querySelector('#channel').value;
-            console.log(channel_name);
-            socket.emit('submit channel', channel_name);
+            const new_channel = document.querySelector('#channel').value
+            socket.emit('update channels', new_channel);
         };
     });
 
-    // When a new channel name is announced, add it to the list
-    socket.on('announce', data => {
-        // console.log(data["channels"])
-        // clear list of channels
-        document.getElementById("channels_list").innerHTML = "";
-        // show list of channels
-        data["channels"].forEach(function (item) {
-            var ul = document.getElementById('channels_list');
-            var li = document.createElement("li");
-            li.innerHTML += item;
-            ul.appendChild(li);
-        });
+    // When channels are sent, update the channel display
+    socket.on('send channels', data => {
+        const channels = data["channels"] 
+        // display list of channels
+        display_channels(channels)
+        // clear field where new channel name was entered
         document.getElementById('channel').value = ''
         console.log("after clearing input")
     });
+
 });
